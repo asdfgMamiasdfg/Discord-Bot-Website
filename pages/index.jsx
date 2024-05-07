30658b6ac4500890dcca07f0f8e98f12ae62f538
asdfgMamiasdfg <houselandesports@gmail.com>
2024-05-08 00:37:29 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,11 +19,7 @@ export default function Index() {
   
             <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
               <a
-                className={
-                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
-                }
-              >
-          Invite SierraBOT
+                
               </a>
             </Link>
   <div className="py-10"></div>
